e221b38d8abd0de8518056214bef4ae340c68eaf
Dmitriy Samofalov <samdimus@gmail.com>
2019-05-12 17:34:51 +0300

    добавлен проект с примерами рекурсивных функций

---
+++ 4_Recursion_7_1/Program.fs
@@ -0,0 +1,12 @@
+﻿let rec fibo = function
+| 0 -> 0
+| 1 -> 1
+| n -> fibo(n-1) + fibo(n-2)
+
+let rec sum = function
+| 1 -> 1
+| n -> sum(n-1) + n
+
+let rec sum2 = function
+| (m,0) -> m
+| (m,n) -> sum2(m, n - 1) + m + n
