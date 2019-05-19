717cf81e80d00153b0e6efb7091151c76c56a9a0
Dmitriy Samofalov <samdimus@gmail.com>
2019-05-19 17:20:39 +0300

    добавлен проект с примерами функций предикатов

---
+++ 5_Predicate_16/Program.fs
@@ -0,0 +1,8 @@
+﻿// 16.1
+let notDivisible (n, m) = m % n = 0 
+// printfn  "%b" (notDivisible(5, 43))
+// 16.2
+let rec prime = function
+| 1 -> false
+| 2 -> true
+// todo написать субфункцию n -> subrec(n)
