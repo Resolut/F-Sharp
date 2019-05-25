a6958104a3a556740ae66e636da61d424443e25a
Dmitriy Samofalov <samdimus@gmail.com>
2019-05-25 18:57:32 +0300

    добавлена вспомогательная рекурсивная функция subrec

--- 5_Predicate_16/Program.fs
+++ 5_Predicate_16/Program.fs
@@ -1,12 +1,13 @@
 ﻿// 16.1
 let notDivisible (n, m) = m % n = 0 
-// printfn  "%b" (notDivisible(5, 43))
+
 // 16.2
+let rec subrec = function
+| (n, i) when n = i -> true
+| (n, i) when (n % i) = 0 -> false 
+| (n, i) -> subrec(n, i + 1)
+
 let rec prime = function
 | 1 -> false
 | 2 -> true
-| n -> subrec(n, n-1)// todo написать субфункцию n -> subrec(n)
-
-let rec subrec = function
-|
-|
+| n -> subrec(n, 2)
