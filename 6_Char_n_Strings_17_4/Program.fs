2d387db8301a0916e73b9a08687fde5ae8f3a7b8
Dmitriy Samofalov <samdimus@gmail.com>
2019-05-28 18:55:30 +0300

    добавлен код функций pow и isIthChar

--- 6_Char_n_Strings_17_4/Program.fs
+++ 6_Char_n_Strings_17_4/Program.fs
@@ -1,8 +1,19 @@
 ﻿// 17.1
-//let rec pow  
+let rec pow = function
+| (s, 0) -> ""
+| (s, 1) -> s
+| (s, n) -> s + pow(s, n-1)
+
+//printfn "%s" (pow("1", 1))
 
 // 17.2
-//let rec isIthChar  
+let isIthChar (s:string, n, c) =
+    if n >= 0 && n <= s.Length-1 then s.[n] = c 
+    else false
+
+printfn "%b" (isIthChar("skillsmart", 2, 'i'));
 
 // 17.3
 //let rec occFromIth 
+
+let c = System.Console.ReadKey();
