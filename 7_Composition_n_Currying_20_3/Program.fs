6c936556ae43f7b9082a95771a8c45d19c4e2f3e
Dmitriy Samofalov <samdimus@gmail.com>
2019-06-25 18:46:41 +0300

    добавлен код функций vat и unvat

--- 7_Composition_n_Currying_20_3/Program.fs
+++ 7_Composition_n_Currying_20_3/Program.fs
@@ -1,8 +1,13 @@
 ﻿// 20.3.1
-//let vat n x = 
+let vat n x = x + x * float(n) / 100.0
+let num = vat 15
+printfn "%f" (num 100.0)
 
 // 20.3.2
-//let unvat n x =
+let unvat n x = x * 100.0 / float(100 + n)
+printfn "%f" (unvat 15 (vat 15 100.0))
 
 // 20.3.3
 //let rec min f =
+
+System.Console.ReadKey();
