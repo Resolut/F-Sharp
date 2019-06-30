6d3a7ec38000434714713b1bac92adfb792a1b8a
Dmitriy Samofalov <samdimus@gmail.com>
2019-06-30 17:15:10 +0300

    Изменен код функции min (f): данная функция использует вспомогательную рекурсивную функцию sub с начальным параметром равным 0

--- 7_Composition_n_Currying_20_3/Program.fs
+++ 7_Composition_n_Currying_20_3/Program.fs
@@ -1,19 +1,12 @@
 ﻿// 20.3.1
 let vat n x = x + x * float(n) / 100.0
-//let num = vat 15
-//printfn "%f" (num 100.0)
 
 // 20.3.2
 let unvat n x = x * 100.0 / float(100 + n)
-//printfn "%f" (unvat 15 (vat 15 100.0))
 
 // 20.3.3
-let rec min f = 
-    let n = 0
-    if f n = 0 then n
-    else f (n+1)
-
-let num x = x - 5  
-printfn "%d" (min (num))
-
-System.Console.ReadKey();
+let min f = 
+    let rec sub = function
+    | n when f n = 0 -> n
+    | n -> sub (n+1)
+    sub 0
