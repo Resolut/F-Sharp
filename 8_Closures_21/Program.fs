edbd41577b991bfa7a45b1633dd4b47365c7db74
Dmitriy Samofalov <samdimus@gmail.com>
2019-07-11 16:24:15 +0300

    добавлены комментарии и примеры использования функций curry и uncurry

--- 8_Closures_21/Program.fs
+++ 8_Closures_21/Program.fs
@@ -1,5 +1,29 @@
 ﻿// 21.1
+// вариант 1
+let curry2 f =
+    let g = fun x y -> f (x, y)
+    g
+
+// вариант 2 
 let curry f = fun x y -> f (x, y)
 
+//пример использования curry
+let mul (a, b) = a * b
+let res = curry mul
+printfn "%d" (res 10 5)
+
 // 21.2
-let uncurry f = fun (x, y) -> f x y 
+// вариант 1
+let uncurry f =
+    let g = fun (x, y) -> f x y
+    g
+
+// вариант 2
+let uncurry2 f = fun (x, y) -> f x y 
+
+//пример использования uncurry
+let add x = fun y -> x + y
+let res2 = uncurry add
+printfn "%d" (res2 (7, 5)) 
+
+System.Console.ReadKey() |> ignore; // ожидание нажатия клавиши
