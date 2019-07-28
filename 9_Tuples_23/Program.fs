6b86827615a4a2ee8cbd8e92f813963c55bb2e15
Dmitriy Samofalov <samdimus@gmail.com>
2019-07-28 17:42:51 +0300

    добавлены операторы для вычитания игровой валюты и операторы для арифметических операций с комплексными числами

--- 9_Tuples_23/Program.fs
+++ 9_Tuples_23/Program.fs
@@ -7,21 +7,44 @@ let (.+.) x y =
     let g = g1 + g2 + (s1 + s2 + (c1 + c2) / 12) / 20
     (g, s, c)
 
-printfn " Золото Серебро Медь \n 1 Золото = 20 Серебряных\n 1 Серебряный = 12 Медяков"
-printfn " Игровой баланс: %A + %A = %A" (5, 6, 12) (1, 20, 7) ((5, 6, 12) .+. (1, 20, 7))
-
 let (.-.) x y = 
     let (g1, s1, c1) = x
     let (g2, s2, c2) = y
-    let c = c1 - c2
-    let s = s1 - s2
-    let g = (g1 - g2) > 0 
-    (g,s,c)
+
+    let c =  
+        if (c1 >= c2) then (c1-c2) % 12
+        else c1 - c2
+
+    let s = 
+        if (s1 >= s2) && (c1 >= c2) then (s1 - s2 + (c1 - c2) / 12) % 20
+        elif (s1 >= s2) && (c1 < c2) then (s1-s2) % 20
+        elif (s1 < s2) && (c1 >= c2) then (s1 - s2) + (c1 - c2) / 12 
+        else s1 - s2
+
+    let g = 
+        if (s >= 0) && (c >= 0) then g1 - g2 + (s1 - s2 + (c1 - c2) / 12) / 20
+        elif (s >= 0) && (c < 0) then g1 - g2 + (s1 - s2) / 20
+        else g1 - g2
+    (g, s, c)
 
 // 23.4.2 Арифметические операции с комплексными числами
 let (.+) x y = 
-//let (.-) x y = 
-//let (.*) x y = 
-//let (./) x y = 
+    let (a, b) = x
+    let (c, d) = y
 
-System.Console.ReadKey();
+    (a + c, b + d)  
+
+let (.-) x y =
+    let (a, b) = x
+    let (c, d) = y
+    (a - c, b - d)
+
+let (.*) x y = 
+    let (a, b) = x
+    let (c, d) = y
+    (a * c - b * d, b * c + a * d)
+
+let (./) x y = 
+    let (a, b) = x
+    let (c, d) = y
+    ((a * c + b * d) / (c * c + d * d) ,  (b * c - a * d) / (c * c + d * d))
