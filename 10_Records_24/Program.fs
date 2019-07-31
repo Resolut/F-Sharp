26b88da1fc4f1d6b37e3acb92a510e69f28d2a64
Dmitriy Samofalov <samdimus@gmail.com>
2019-07-31 18:01:15 +0300

    Добавлен проект Записи (Records)

---
+++ 10_Records_24/Program.fs
@@ -0,0 +1,11 @@
+﻿type TimeOfDay = {hours : int; minutes: int; f : string}
+
+let (.>.) x y =
+    let time1 = x    
+    let time2 = y
+    let time1toMin = time1.hours * 60 + time1.minutes
+    let time2toMin = time2.hours * 60 + time2.minutes
+    
+    if (time1.f = "AM" && time2.f = "PM") then false
+    elif (time1.f = "PM" && time2.f = "AM") then true
+    else time1toMin > time2toMin
