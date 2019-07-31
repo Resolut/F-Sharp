c32cb5ff3d5ffdbf57e2884f9b401a85f494f77f
Dmitriy Samofalov <samdimus@gmail.com>
2019-07-31 16:43:33 +0300

    изменен код оператора .-.

--- 9_Tuples_23/Program.fs
+++ 9_Tuples_23/Program.fs
@@ -12,25 +12,16 @@ let (.-.) x y =
     let (g1, s1, c1) = x
     let (g2, s2, c2) = y
 
-    let c =  
-        if (c1 >= c2) then (c1-c2) % 12
-        else 0
-
-    let s = 
-        if (s1 >= s2) && (c1 >= c2) then (s1 - s2 + (c1 - c2) / 12) % 20
-        elif (s1 >= s2) && (c1 < c2) then (s1-s2) % 20
-        elif (s1 < s2) && (c1 >= c2) then (c1 - c2) / 12 
-        else 0
-
-    let g = 
-        if (g1 >= g2) && (s1 >= s2) && (c1 >= c2) then (g1 - g2) + (s1 - s2 + (c1 - c2) / 12) / 20
-        elif (g1 >= g2) && (s1 >= s2) && (c1 < c2) then (g1 - g2) + (s1 - s2) / 20
-        elif (g1 >= g2) && (s1 < s2) && (c1 >= c2) then (g1 - g2) + (c1 - c2) / 12 / 20
-        elif (g1 >= g2) && (s1 < s2) && (c1 <  c2) then g1 - g2
-        elif (g1 < g2) && (s1 >= s2) && (c1 >=  c2) then (s1 - s2 + (c1 - c2) / 12) / 20
-        elif (g1 < g2) && (s1 >= s2) && (c1 <  c2) then (s1 - s2) / 20
-        elif (g1 < g2) && (s1 < s2) && (c1 >=  c2) then (c1 - c2) / 12 / 20
-        else 0
+    let c11 = c1 % 12
+    let c22 = c2 % 12
+    let s11 = (s1 + c1 / 12) % 20 
+    let s22 = (s2 + c2 / 12) % 20 
+    let g11 = g1 + (s1 + c1 / 12) / 20
+    let g22 = g2 + (s2 + c2 / 12) / 20
+    
+    let c = if (c11 > c22) then c11 - c22 else 0
+    let s = if (s11 > s22) then s11 - s22 else 0
+    let g = if (g11 > g22) then g11 - g22 else 0
     (g, s, c)
 
 // 23.4.2 Арифметические операции с комплексными числами
