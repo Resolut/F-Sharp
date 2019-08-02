26e5c38a965804413a4ab171e4aeb852cfaff0e0
Dmitriy Samofalov <samdimus@gmail.com>
2019-08-02 19:04:23 +0300

    добавлен код функций upto, dnto, evenn

--- 12_Lists_34/Program.fs
+++ 12_Lists_34/Program.fs
@@ -1,8 +1,19 @@
 ﻿// 34.1
-//let rec upto = ...
+let rec upto n =
+    let rec count = function
+        | m when m = n -> [n]
+        | m -> m :: count (m + 1) 
+    count 1
 
 // 34.2
-//let rec dnto = ...
+let rec dnto = function 
+| 0 -> []
+| n -> n :: dnto (n-1)
 
 // 34.3
-//let rec evenn = 
+let rec evenn n = 
+    let rec count = function 
+        | (num, acc) when acc = n -> [] 
+        | (num, acc) when (num % 2 = 0) -> num :: count (num + 1,acc + 1)  
+        | (num, acc) -> count (num + 1, acc) 
+    count (0, 0)
