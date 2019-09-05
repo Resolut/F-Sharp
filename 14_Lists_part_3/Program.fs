f490bf7bddc10a1e267ff0e72b736625a608bf9f
Dmitriy Samofalov <samdimus@gmail.com>
2019-09-05 10:00:55 +0300

    Добавлен проект с заготовками функций

---
+++ 14_Lists_part_3/Program.fs
@@ -0,0 +1,31 @@
+﻿// 40.1
+//let rec sum (p, xs) = 
+
+// 40.2.1
+//let rec count (xs, n) = ...
+
+// 40.2.2
+//let rec insert (xs, n) = ...
+
+// 40.2.3
+//let rec intersect (xs1, xs2) = ...
+
+// 40.2.4
+//let rec plus (xs1, xs2) = ...
+
+// 40.2.5
+//let rec minus (xs1, xs2) = ...
+
+// 40.3.1
+//let rec smallest = ...
+
+// 40.3.2
+//let rec delete (n, xs) = ...
+
+// 40.3.3
+//let rec sort = ...
+
+// 40.4
+//let rec revrev = ...
+
+System.Console.ReadKey |> ignore
