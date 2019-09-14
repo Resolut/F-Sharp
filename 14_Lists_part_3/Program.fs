e75932af51d6ffa8480a338c351f327a5de49563
Dmitriy Samofalov <samdimus@gmail.com>
2019-09-14 18:23:00 +0300

    отформатирован код, убраны комментарии результатов вывода функций

--- 14_Lists_part_3/Program.fs
+++ 14_Lists_part_3/Program.fs
@@ -59,7 +59,6 @@ let rec iter_minus (xs1, xs2, list) =
 
 let rec minus (xs1, xs2) = iter_minus (xs1, xs2, [])
 
-
 // 40.3.1 - функция smallest: int list -> int возвращает наименьший элемент непустого списка.
 let rec smallest = function
 | [] -> failwith "Невозможно определить наименьший элемент пустого списка"
@@ -70,8 +69,7 @@ let rec smallest = function
         | head :: tail when head < min -> compare head tail 
         | head :: tail -> compare min tail 
     compare head tail
-    
-
+ 
 // 40.3.2 - функция delete: int * int list -> int list удаляет из списка первое вхождение заданного элемента (если он имеется).
 let rec delete (n, xs) = 
     let rec sub_delete xs =
@@ -81,90 +79,13 @@ let rec delete (n, xs) =
         | head :: tail -> head :: sub_delete tail
     sub_delete xs
 
-
-// 40.3.3 - функция sort использует предыдущие функции и сортирует входной список так, что на выходе получается слабо восходящий список.
+// 40.3.3 - функция sort: использует предыдущие функции и сортирует входной список так, что на выходе получается слабо восходящий список.
 let rec sort = function
 | [] -> []
 | list -> smallest (list) :: sort (delete (smallest (list), list))
 
 
-// 40.4 - функция revrev
+// 40.4 - функция revrev: получает на вход список списков, и перевёртывает как порядок вложенных списков, так и порядок элементов внутри каждого вложенного списка.
 let rec revrev = function
 | [] -> []
 | head :: tail -> revrev tail @ [(List.rev head)] 
-
-
-// vvvvvv Вывод результатов работы функций vvvvvv
-
-// 40.1 функция sum
-//let isEven = function 
-//| elem -> elem % 2 = 0
-
-//let isOdd = function 
-//| elem -> elem % 2 = 1
-
-//printfn "%A" (sum (isEven, [1;2;3;4]))
-//printfn "%A" (sum (isOdd, [1;1;2;3;4;5;]))
-
-// 40.2.1 функция count
-//printfn "%A" (count ([1;2;3;4],5))
-
-// 40.2.2 - функция insert
-//printfn "%A" (insert ([1;2;3;4], 5))
-//printfn "%A" (insert ([1;2;3;4], 0))
-//printfn "%A" (insert ([1;2;3;4], 1))
-//printfn "%A" (insert ([1;2;3;3;6], 4))
-//printfn "%A" (insert ([], 4))
-
-// 40.2.3 - функция intersect
-//printfn "%A" (intersect ([5;6;7], [1;2;3;4]))
-//printfn "%A" (intersect ([4;5;6;7], [1;2;3;4]))
-//printfn "%A" (intersect ([4;5;6;7], [2;3;4;5]))
-//printfn "%A" (intersect ([4;5;6;7], [4;5]))
-//printfn "%A" (intersect ([6;7], [4;5;6;7]))
-//printfn "%A" (intersect ([4;5;6;7], [4;5;6;7]))
-
-// 40.2.4 - функция plus
-//printfn "%A" (plus ([1; 2; 3; 4], [5; 6; 7; 8; 9]))
-//printfn "%A" (plus ([5; 6; 7; 8; 9], [1; 2; 3; 4]))
-//printfn "%A" (plus ([1; 2; 3; 4], [1; 2; 3; 4]))
-//printfn "%A" (plus ([1; 2; 3; 4], []))
-//printfn "%A" (plus ([], [5;5;6;7]))
-//printfn "%A" (plus ([1; 2; 3; 4], [2; 2; 6]))
-//printfn "%A" (plus ([], []))
-
-// 40.2.4 - функция minus
-//printfn "%A" (minus ([6; 7], [1; 6]))
-//printfn "%A" (minus ([1; 2; 3; 4], [1; 2; 3; 4]))
-//printfn "%A" (minus ([1; 2; 3; 4; 4; 4], [1; 2; 3; 4]))
-//printfn "%A" (minus ([1; 2; 3; 4; 4; 4], [4; 5]))
-//printfn "%A" (minus ([1; 2; 3; 4], [4]))
-//printfn "%A" (minus ([1; 2; 3; 4], [5; 6; 7; 8; 9]))
-//printfn "%A" (minus ([1; 2; 3; 4], [3; 6]))
-//printfn "%A" (minus ([1; 2; 3; 4; 5], [3; 4; 5]))
-//printfn "%A" (minus ([1; 2; 3; 4; 5], []))
-
-// 40.3.1 - функция smallest
-//printfn "%A" (smallest [1; 6; 184; 2;-11 ; -303; 27; 0])
-//printfn "%A" (smallest [1; 6; 184; 2; 27; 0])
-//printfn "%A" (smallest [3; 6; 184; 5; 27; 3])
-
-// 40.3.2 - функция delete
-//printfn "%A" (delete (5, [1; 2; 3; 4; 5; 5; 7]))
-//printfn "%A" (delete (5, [40; 30; 5; 5; 7; 5]))
-//printfn "%A" (delete (5, [50; 40; 30; 20; 10; 0]))
-
-// 40.3.3 - функция sort
-//printfn "%A" (sort [])
-//printfn "%A" (sort [1])
-//printfn "%A" (sort [50; 40; 30; 20; 10; 0])
-//printfn "%A" (sort [22; 18; 25; 19; 24; 20; 23; 21])
-//printfn "%A" (sort [8; -8; 9; -9; 7; -7; 0; -1; 1])
-//printfn "%A" (sort [8; 8; 9; 9; 7; 7; 0; 1; 1])
-
-// 40.4 - функция revrev
-//printfn "%A" (revrev ([[1; 2]; [3; 4; 5]]))
-//printfn "%A" (revrev [[6;5;4];[3;2;1]])
-//printfn "%A" (revrev [[9;8;7];[6;5;4];[3;2;1]])
-
-System.Console.ReadKey |> ignore
