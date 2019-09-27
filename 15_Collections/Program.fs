ce38d468b5c3e5f510a0ba8277d7e66d60a0c89e
Dmitriy Samofalov <samdimus@gmail.com>
2019-09-27 14:58:24 +0300

    Добавлен код функции revrev. Добавлены комментарии к функциям

--- 15_Collections/Program.fs
+++ 15_Collections/Program.fs
@@ -1,18 +1,12 @@
-﻿// 41.4.1  
+﻿// 41.4.1 - функция filter: (int -> bool) * int list -> int list
+// возвращает список элементов исходного списка для которых истинен предикат f  
 let list_filter f xs = List.foldBack (fun head xs -> if f head then head :: xs else xs) xs []
 
-printfn "%A" (list_filter(fun x -> x > 0) [-1; 2; 3])
-printfn "%A" (list_filter(fun x -> x % 2 = 0) [-1; 0; 2; 65; 3; 31; 4; 49; 8; 16; 32; 64])
-
-// 41.4.2
+// 41.4.2 - функция sum: (int -> bool) * int list -> int
+// возвращает сумму элементов списка xs, для которых истинен предикат p.
 let sum (p, xs) = List.foldBack (fun head acc -> if p head then acc + head else acc) xs 0
 
-printfn "%A" (sum ((fun x -> x > 0), [-1; -2; 3]))
-printfn "%A" (sum ((fun x -> x < 0), [-1; -2; 3]))
-printfn "%A" (sum ((fun x -> x < 0), [0; 1; 1; 1; -1; -2; -3]))
-printfn "%A" (sum ((fun x -> x > 0), [0; 1; 1; 1; -1; -2; 3]))
-
-// 41.4.3
-//let revrev = ...
-
-System.Console.ReadKey() |> ignore
+// 41.4.3 - функция revrev: int list list -> int list list 
+// получает на вход список списков, и перевёртывает как порядок вложенных списков, 
+// так и порядок элементов внутри каждого вложенного списка.
+let revrev = fun lst -> List.fold (fun head tail -> List.fold (fun head tail -> tail :: head) [] tail :: head) [] lst
