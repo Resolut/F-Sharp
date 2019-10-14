dd601606bce8a576576450003ec27d0d496cac9b
Dmitriy Samofalov <samdimus@samdimus@gmail.com>
2019-10-14 18:22:48 +0300

    добавлен TODO комментарий

--- 16_Sets/Program.fs
+++ 16_Sets/Program.fs
@@ -25,7 +25,7 @@ let rec allSubsets n k =
 
     let rec sub_set lst acc=
         match lst, acc with
-        | (h::t), acc when acc > 1  -> [h::t] @ sub_set (h::[t.Head+1]) (acc-1)
+        | (h::t), acc when acc > 1  -> [h::t] @ sub_set (h::[t.Head+1]) (acc-1) // TODO: попробовать манипулировать через tail.Head
         | (h::t), acc -> [h::t]
         | [], acc -> [[]]
     sub_set startList n
