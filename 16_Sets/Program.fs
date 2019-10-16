f1a6c452165132dc04594813c2dce1abb107d8b3
Dmitriy Samofalov <samdimus@samdimus@gmail.com>
2019-10-16 18:16:50 +0300

    функция create переименована в create_range изменен код функции allSubsets; добавлены комментарии

--- 16_Sets/Program.fs
+++ 16_Sets/Program.fs
@@ -1,38 +1,19 @@
-﻿// 42.3 
-let rec create k =
-    let rec sub n =
-        match n with
-        | n when n < k -> Set.add (n + 1) (sub (n+1))
-        | n -> set[n]
-    sub 0
-
-//printfn "%A" (create 1)
-//printfn "%A" (create 2)
-//printfn "%A" (create 3)
-//printfn "%A" (create 4)
-//printfn "%A" (create 9)
-
-// 34.1
-let rec upto n =
+﻿// вспомогательная функция create_range : int -> int list 
+let rec create_range n = 
     let rec count = function
         | m when m = n -> [n]
         | m -> m :: count (m + 1) 
     count 1
 
-//нерабочий код
+//42.3 функция allSubsets int -> int -> Set<Set<int>>
+// возвращает множество всех подмножеств множества {1, 2, ..., n},
+// в которых ровно k элементов (0 < k < n)
 let rec allSubsets n k = 
-    let startList = upto k // создание начального списка
-    // TODO: написать функцию для изменения списка 
-    let rec sub_set lst acc=
-        match lst, acc with
-        | (h::t), acc when acc > 0  -> [h::t] @ sub_set (h::[t.Head+1]) (acc-2)  
-        | (h::t), acc -> [h::t]
-        | [], acc -> [[]]
-    sub_set startList n
-    // TODO: преобразование списка в множество
+    let rec superset = function
+        | [] -> [[]]
+        | x :: xs -> 
+            let ps = superset xs in
+            ps @ List.map (fun ss -> x :: ss) ps
 
-printfn "%A" (allSubsets 5 2)
-printfn "%A" (allSubsets 5 3)
-
-
-System.Console.ReadKey() |> ignore
+    let filtered_lst = List.filter (fun lst -> List.length lst = k) (superset (create_range n))
+    Set.ofList(List.map (fun lst -> Set.ofList (lst)) filtered_lst)
