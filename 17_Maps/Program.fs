e67ae241ef3eac6faedea7daff6bab27b6c5e3a6
Dmitriy Samofalov <samdimus@samdimus@gmail.com>
2019-10-17 11:11:29 +0300

    добавлен код функции try_find

--- 17_Maps/Program.fs
+++ 17_Maps/Program.fs
@@ -1,7 +1,10 @@
-﻿// Learn more about F# at https://fsharp.org
-// See the 'F# Tutorial' project for more help.
-
-[<EntryPoint>]
-let main argv =
-    printfn "%A" argv
-    0 // return an integer exit code
+﻿// 43.3 функция try_find : a -> Map<a,b> -> b option 
+// возвращает значение из отображения m по ключу key или None если ключ не найден
+let try_find key m = 
+    let lst = Map.toList m
+    let rec sub_try lst =
+        match lst with
+        | [] -> None
+        | (k1,v1)::(tail) when k1 = key -> Some(v1) 
+        | (k1,v1)::(tail)-> sub_try tail
+    sub_try lst
