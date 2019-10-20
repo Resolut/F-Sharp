9c00cfedc18b1d33d00bbfd903a29c1b56e96885
Dmitriy Samofalov <samdimus@gmail.com>
2019-10-20 15:32:19 +0300

    Добавлен код функций f и fibo

--- 18_Imperative_Programming/Program.fs
+++ 18_Imperative_Programming/Program.fs
@@ -1,5 +1,21 @@
 ﻿// 47.4.1 функция f: int -> int вычисляет факториал числа n
-//let f n = 
+let f n =
+    let mutable sum = 1
+    let x = ref 1
+    while ! x <= n do
+        sum <- sum * !x
+        x := ! x + 1
+    sum
 
 // 47.4.2 функция fibo: int -> int вычисляет n-ое число Фибоначчи
-//let fibo n = ...
+let fibo n = 
+    let mutable num = 1
+    let x = ref 1
+    let a = ref 0
+    let b = ref 1
+    while ! x <= n do
+        num <- ! a + ! b
+        a := ! b
+        b := num
+        x := ! x + 1
+    ! a
