f2c72858b0cefdca28b56e365d4d0a2cf739e122
Dmitriy Samofalov <samdimus@gmail.com>
2019-10-20 16:03:35 +0300

    Добавлен шаблон кода функций fibo и bigList

--- 19_Lazy_Computing/Program.fs
+++ 19_Lazy_Computing/Program.fs
@@ -1,7 +1,11 @@
-﻿// Learn more about F# at https://fsharp.org
-// See the 'F# Tutorial' project for more help.
+﻿// 48.4.1 fibo1: int -> int -> int -> int, 
+// с двумя параметрами-аккумуляторами n1 и n2, где
+// fibo1 n n1 n2 = Fn, n1 = Fn-1 и n2 = Fn-2
+// let rec fibo1 n n1 n2 = ...
 
-[<EntryPoint>]
-let main argv =
-    printfn "%A" argv
-    0 // return an integer exit code
+// 48.4.2 fibo2: int -> (int -> int) -> int,
+// где первый параметр -- это функция-продолжение
+//let rec fibo2 c n = ...
+
+// 48.4.3 bigList : функция генерации списка
+//let rec bigList n k = ...
