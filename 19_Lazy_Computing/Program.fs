d5bec01e7ad77fefd9ceaf20926a61b30d643d79
Dmitriy Samofalov <samdimus@samdimus@gmail.com>
2019-10-24 09:44:38 +0300

    изменен код функции fibo1,  код отформатитрован

--- 19_Lazy_Computing/Program.fs
+++ 19_Lazy_Computing/Program.fs
@@ -2,14 +2,15 @@
 // с двумя параметрами-аккумуляторами n1 и n2, где
 //fibo1 n n1 n2 = Fn, n1 = Fn-1 и n2 = Fn-2
 let rec fibo1 n n1 n2 = 
-    if n <= 2 then n2
-    else fibo1 (n-1) n2 (n1 + n2)
+    if n = 0 then n
+    elif n < 2 then (n1 + n2)
+    else fibo1 (n - 1) n2 (n1 + n2)
 
-// 48.4.2 fibo2: int -> (int -> int) -> int,
+// 48.4.2 fibo2: (int -> int) -> int -> int,
 // где первый параметр -- это функция-продолжение
 let rec fibo2 c n = 
     if n < 2 then c n
-    else fibo2 (fun x -> fibo2 (fun y -> c (x + y)) (n-2) ) (n-1)
+    else fibo2 (fun x -> fibo2 (fun y -> c (x + y)) (n - 2) ) (n - 1)
 
 // 48.4.3 bigList : функция генерации списка
 let rec bigList n k =     
