eef75862ac3615730df0ec50fcd4ef7a173972d4
Dmitriy Samofalov <samdimus@gmail.com>
2019-10-27 20:15:53 +0300

    функция fac_sec нерабочая изменен код функции seq_seq

--- 21_Sequence_Expression/Program.fs
+++ 21_Sequence_Expression/Program.fs
@@ -3,14 +3,23 @@ let fac_seq =
     let factorial n = 
         let rec f x a = seq {
             if x <= 1 then yield a 
-            else yield! f (x - 1) (a * x) } 
+            else yield! f (x - 1) (a * x) } //   
         f n 1 
-    factorial
+    factorial 
+
+let fac_seq1 = Seq.initInfinite fac_seq 
+let cache_fac_seq = Seq.cache fac_seq1
+printfn "%A" (cache_fac_seq)
 
 // 50.2.2
-let seq_seq = 
-    let rec check_num n = seq {
-        match n with 
-        | n when n % 2 = 0 -> yield (n / 2)
-        | n -> yield (n / 2 - n) }
-    check_num
+let seq_seq =
+    let inner n = seq {
+        for x in 0..n do 
+          if x % 2 = 0 then yield (x / 2)
+          else yield (x / 2 - x) }
+    inner 
+
+let cache_seq_seq = Seq.cache (seq_seq 10)
+printfn "%A" (cache_seq_seq)
+
+System.Console.ReadKey() |> ignore
