1bcea932efeb7ec7bee8e7265b7f5708fa7ea0b8
Dmitriy Samofalov <samdimus@gmail.com>
2019-10-27 16:15:54 +0300

    Добавлен код функций even_seq, fac_seq и seq_seq

--- 20_Lazy_Sequences/Program.fs
+++ 20_Lazy_Sequences/Program.fs
@@ -1,9 +1,19 @@
 ﻿// 49.5.1
-//let even_seq = ...
+let even_seq = Seq.initInfinite (fun i -> (2*i+2)) 
 
-//printfn "%A" (even_seq)
 // 49.5.2
-//let fac_seq = ...
+let factorial n =
+    let rec f x a =
+        if x <= 1 then a
+        else f (x - 1) (a * x)
+    f n 1
+
+let fac_seq = Seq.initInfinite factorial 
 
 // 49.5.3
-//let seq_seq = ...
+let check_num n = 
+    if n = 0 then 0
+    elif n % 2 = 0 then (n / 2)
+    else n / 2 - n
+        
+let seq_seq = Seq.initInfinite check_num
