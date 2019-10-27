cd4394c1266233bf7410ca4006e1d74d3f57f09c
Dmitriy Samofalov <samdimus@gmail.com>
2019-10-27 17:49:59 +0300

    добавлен код функций fac_seq и seq_seq

--- 21_Sequence_Expression/Program.fs
+++ 21_Sequence_Expression/Program.fs
@@ -1,16 +1,16 @@
 ﻿// 50.2.1
 let fac_seq = 
-    let factorial n =
-        let rec f x a =
-            if x <= 1 then a
-            else f (x - 1) (a * x)
-        f n 1
-    Seq.initInfinite factorial
+    let factorial n = 
+        let rec f x a = seq {
+            if x <= 1 then yield a 
+            else yield! f (x - 1) (a * x) } 
+        f n 1 
+    Seq.initInfinite (factorial) 
 
 // 50.2.2
 let seq_seq = 
-    let rec check_num n =
+    let rec check_num n = seq {
         match n with 
-        | n when n % 2 = 0 -> (n / 2)
-        | n -> n / 2 - n
+        | n when n % 2 = 0 -> yield (n / 2)
+        | n -> yield (n / 2 - n) }
     Seq.initInfinite check_num
