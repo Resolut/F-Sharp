ab71ae2840ce8c527d789bee81ef7948c0e6a7aa
Dmitriy Samofalov <samdimus@yandex.ru>
2019-10-28 10:52:56 +0300

    обновлены комментарии к функциям

--- 19_Lazy_Computing/Program.fs
+++ 19_Lazy_Computing/Program.fs
@@ -1,18 +1,22 @@
-﻿// 48.4.1 fibo1: int -> int -> int -> int, 
-// с двумя параметрами-аккумуляторами n1 и n2, где
-//fibo1 n n1 n2 = Fn, n1 = Fn-1 и n2 = Fn-2
+﻿// 48.4.1 функция fibo1: int -> int -> int -> int, 
+// вычисляет n-ое число последовательности Фибоначчи,
+// с помощью двух параметров-аккумуляторов n1 и n2, где
+// fibo1 n n1 n2 = Fn, n1 = Fn-1 и n2 = Fn-2
 let rec fibo1 n n1 n2 = 
     if n = 0 then n
     elif n < 2 then (n1 + n2)
     else fibo1 (n - 1) n2 (n1 + n2)
 
-// 48.4.2 fibo2: (int -> int) -> int -> int,
-// где первый параметр -- это функция-продолжение
+// 48.4.2 функция fibo2: (int -> int) -> int -> int,
+// вычисляет n-ое число последовательности Фибоначчи,
+// где параметр c- это функция-продолжение,
+// n - порядковый номер числа
 let rec fibo2 c n = 
     if n < 2 then c n
     else fibo2 (fun x -> fibo2 (fun y -> c (x + y)) (n - 2) ) (n - 1)
 
-// 48.4.3 bigList : функция генерации списка
+// 48.4.3 функция bigList: int -> (int list -> int list) -> int lis 
+// генерирует список вида [1;1;1;...] размером n
 let rec bigList n k =     
     let rec f acc lst =
         if acc = 0 then k lst
