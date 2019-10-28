2a42667d5a61446e1fac27153bae020ef013c40f
Dmitriy Samofalov <samdimus@yandex.ru>
2019-10-28 10:30:09 +0300

    изменен код функций, добавлены комментарии

--- 21_Sequence_Expression/Program.fs
+++ 21_Sequence_Expression/Program.fs
@@ -1,25 +1,19 @@
-﻿// 50.2.1
+﻿// 50.2.1 функция fac_seq : seq<int>
+// определяет последовательность факториалов
+// неотрицательных чисел {1, 1, 2, 6, 24, ...}
 let fac_seq = 
-    let factorial n = 
-        let rec f x a = seq {
-            if x <= 1 then yield a 
-            else yield! f (x - 1) (a * x) } // wrong line   
-        f n 1 
-    factorial 
+    let rec factorial n acc = seq {
+        if n <= 1 then yield 1
+        else yield acc
+        yield! factorial (n + 1) (acc * (n+1)) }
+    factorial 0 1
 
-let fac_seq1 = Seq.initInfinite fac_seq 
-let cache_fac_seq = Seq.cache fac_seq1
-printfn "%A" (cache_fac_seq)
-
-// 50.2.2
-let seq_seq =
-    let inner n = seq {
-        for x in 0..n do 
-          if x % 2 = 0 then yield (x / 2)
-          else yield (x / 2 - x) }
-    inner 
-
-let cache_seq_seq = Seq.cache (seq_seq 10)
-printfn "%A" (cache_seq_seq)
-
-System.Console.ReadKey() |> ignore
+// 50.2.2 функция seq_seq : seq<int> 
+// определяет последовательность вида
+// {0, -1, 1, -2, 2, -3, 3, ...}
+let seq_seq = 
+    let rec inner n = seq {
+        if n % 2 = 0 then yield (n / 2)
+        else yield (-(n + 1) / 2)
+        yield! inner (n + 1) }
+    inner 0
