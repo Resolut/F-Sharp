6ad13067174aa73531210e8f4ff9a1f97be3d744
Dmitriy Samofalov <samdimus@yandex.ru>
2019-10-28 17:13:29 +0300

    добавлен черновой код функции nth.

--- 22_Lazy_Evaluations/Program.fs
+++ 22_Lazy_Evaluations/Program.fs
@@ -11,22 +11,31 @@ let tl (s : 'a cell) : Lazy<'a cell> =
   | Cons (_, g) -> g
 
 
-let rec li_nth lst n = 
-    let rec inner tempList count = 
-        match tempList, count with
-        | [], _ -> None
-        | h::t, count when count = n -> Some h
-        | h::t, count -> inner t (count + 1)
-    inner lst 0
+//let rec li_nth lst n = 
+//    let rec inner tempList count = 
+//        match tempList, count with
+//        | [], _ -> None
+//        | h::t, count when count = n -> Some h
+//        | h::t, count -> inner t (count + 1)
+//    inner lst 0
 
 // 51.3 функция nth: (s : 'a cell) -> (n : int) -> 'a
 // возвращает n-й (n >= 0) вычисленный элемент бесконечного списка
+//let rec nth (s : 'a cell) (n : int) : 'a =
+//    let rec inner lst count = 
+//        match lst, count with
+//        Nil, _ -> Nil
+//        | Cons (hd,tl), count when count = n -> hd
+//        | Cons (hd,tl), count -> inner (tl.Force()) (count + 1)
+//    inner s 0
+
 let rec nth (s : 'a cell) (n : int) : 'a =
-    let rec inner (tempList: 'a cell) (count:int) = 
-        match tempList, count with
-        Nil, _ -> Nil
-        | Cons (hd,_), count when count = n -> hd
-        | Cons (_,tl), count -> inner (tl.Force()) (count + 1)
+    let rec inner (lst : 'a cell) (count : int) = 
+        match lst with
+        Nil -> Nil
+        | Cons (hd,tl) ->
+            if count = n then hd
+            else (inner (tl.Force()) (count + 1))
     inner s 0
 
 let rec nat (n:int) : 'a cell = Cons (n, lazy(nat(n+1)))
