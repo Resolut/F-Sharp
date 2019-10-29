34433b883dd410761501494ee1205fb5667312e7
Dmitriy Samofalov <samdimus@yandex.ru>
2019-10-29 13:26:15 +0300

    убран тестовый код, добавлены комментарии

--- 22_Lazy_Evaluations/Program.fs
+++ 22_Lazy_Evaluations/Program.fs
@@ -1,51 +1,23 @@
 ﻿// 51.3 
-
+// рекурсивное определение потока как элемента типа 'a cell
 type 'a cell = Nil | Cons of 'a * Lazy<'a cell>
 
+// получение головы потока
 let hd (s : 'a cell) : 'a =
   match s with
     Nil -> failwith "hd"
   | Cons (x, _) -> x
 
+// получение хвоста потока (ленивое вычисление)
 let tl (s : 'a cell) : Lazy<'a cell> =
   match s with
     Nil -> failwith "tl"
   | Cons (_, g) -> g
 
-
-//let rec li_nth lst n = 
-//    let rec inner tempList count = 
-//        match tempList, count with
-//        | [], _ -> None
-//        | h::t, count when count = n -> Some h
-//        | h::t, count -> inner t (count + 1)
-//    inner lst 0
-
 // функция nth: (s : 'a cell) -> (n : int) -> 'a
 // возвращает n-й (n >= 0) вычисленный элемент бесконечного списка
-
-//let rec nth (s : 'a cell) (n : int) : 'a =
-//    let rec inner lst count = 
-//        match lst, count with
-//        Nil, _ -> Nil
-//        | Cons (hd,tl), count when count = n -> hd
-//        | Cons (hd,tl), count -> inner (tl.Force()) (count + 1)
-//    inner s 0
-
 let rec nth (s : 'a cell) (n : int) : 'a =
-    let rec inner (lst : 'a cell) (count : int) = 
-        match lst with
-        | Nil -> hd lst
-        | Cons (hd,tl) ->
-            if count = n then hd
-            else inner (tl.Force()) (count + 1)
+    let rec inner lst (count : int) = 
+        if count = n then hd lst
+        else inner ((tl lst).Force()) (count + 1)
     inner s 0
-
-let rec nat (n:int) : 'a cell = Cons (n, lazy(nat(n+1)))
-let n0 = nat 0
-let n1 = Nil
-
-printfn "%A" (nth n0 30000)
-printfn "%A" (nth n1 0) // Exception hd
-
-System.Console.ReadKey()|> ignore
